8a4194dabfe793aa666be7b813c05587ae43b5d2
Rohit Gupta <r2614476@gmail.com>
2025-05-04 12:06:31 +0000

    u

--- uvx/src/App.jsx
+++ uvx/src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import './App.css'
+
 import Home from './assets/pages/Home.jsx'
  import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
 import AboutSection from './assets/pages/About.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
